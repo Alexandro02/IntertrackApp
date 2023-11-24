00d20620929e5f517f30e56b1b983dcb4e539b83
BEASILROBERTO <joserobertogarciaibanez@outlook.com>
2023-11-24 11:15:38 -0600

    se creo la ubicacion exacta en tiempo real
    la hubicacion en tiempo real

--- lib/widgets/map.dart
+++ lib/widgets/map.dart
@@ -1,26 +1,43 @@
-// ignore_for_file: library_private_types_in_public_api, depend_on_referenced_packages
-
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter/material.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:geolocator/geolocator.dart';
 
 class InterMap extends StatefulWidget {
-  const InterMap({
+ const InterMap({
     Key? key,
-  }) : super(key: key);
+ }) : super(key: key);
 
-  @override
-  _MapState createState() => _MapState();
+ @override
+ _MapState createState() => _MapState();
 }
 
 class _MapState extends State<InterMap> {
-  // Setting up MAP
-  final String apiKey = "E5W2G5zaKeyf8sGShKMGTaUiYABLKNgp";
-  final tomtomHQ = LatLng(25.726232, -100.311603);
-  final tomtomZoom = 18.0;
+ // Setting up MAP
+ final String apiKey = "E5W2G5zaKeyf8sGShKMGTaUiYABLKNgp";
+ final tomtomZoom = 18.0;
+ late LatLng tomtomHQ;
 
-  @override
-  Widget build(BuildContext context) {
+ @override
+ void initState() {
+    super.initState();
+    getCurrentLocation();
+ }
+ //Se utiliza el método getCurrentLocation para obtener las coordenadas actuales del dispositivo
+ Future<void> getCurrentLocation() async {
+    try {
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.high);
+      setState(() {
+        tomtomHQ = LatLng(position.latitude, position.longitude);
+      });
+    } catch (e) {
+      print(e);
+    }
+ }
+
+ @override
+ Widget build(BuildContext context) {
     // Get screen's width and height.
     final double width = MediaQuery.of(context).size.width;
     final double height = MediaQuery.of(context).size.height;
@@ -43,7 +60,7 @@ class _MapState extends State<InterMap> {
               ),
               MarkerLayer(
                 markers: [
-                  Marker(
+                 Marker(
                     width: 80.0,
                     height: 80.0,
                     point: tomtomHQ,
@@ -52,7 +69,7 @@ class _MapState extends State<InterMap> {
                       color: Colors.blue,
                       size: 50.0,
                     ),
-                  ),
+                 ),
                 ],
               ),
             ],
@@ -60,5 +77,5 @@ class _MapState extends State<InterMap> {
         ],
       ),
     );
-  }
+ }
 }
